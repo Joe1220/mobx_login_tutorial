675d1719d23ef6b6fde0b63d1a7b85e16896b2bb
joe1220 <joe1220@daum.net>
2018-10-23 11:38:50 +0900

    기본 mobx observer, observable 사용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { observable } from 'mobx'
+import { observable, action } from 'mobx'
 import { observer } from 'mobx-react'
 
 @observer
@@ -8,20 +8,40 @@ class App extends Component {
   @observable email = ''
   @observable passwords = ''
 
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
   render() {
+    const { email, password } = this
+
     return (
       <div className="App">
         <header>Login</header>
-          <input name="email" placeholder="Email" fluid />
+          <input name="email" placeholder="Email" onChange={this.onChange} value={email} fluid />
           <input
             name="password"
             type="password"
             placeholder="Password"
+            onChange={this.onChange}
+            value={password}
             fluid
           />
-        <button>Submit</button>
+        <button onClick={this.onSubmit}>Submit</button>
       </div>
     );
+  }
+
+  @action.bound
+  onChange(event) {
+    const { name, value } = event.target;
+    this[name] = value;
+  }
+
+  onSubmit() {
+    const { email, password } = this
+    console.log('결과확인 : ', email, password);
   }
 }
 

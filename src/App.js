8b48b811b101efab654a9139c6cac89fb408d827
joe1220 <joe1220@daum.net>
2018-11-24 14:25:37 +0900

    store적용

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import { observable, action } from 'mobx'
-import { observer } from 'mobx-react'
+import React from "react";
+import createBrowserHistory from "history/createBrowserHistory";
+import { Provider } from "mobx-react"
+import { Router, Route, Switch } from "react-router-dom";
 
-@observer
-class App extends Component {
-  @observable email = ''
-  @observable passwords = ''
+import Login from "./pages/Login"
+import { stores } from "./stores"
 
-  constructor(props) {
-    super(props)
-    this.onSubmit = this.onSubmit.bind(this)
-  }
+const history = createBrowserHistory()
 
+class App extends React.Component {
   render() {
-    const { email, password } = this
-
     return (
-      <div className="App">
-        <header>Login</header>
-          <input name="email" placeholder="Email" onChange={this.onChange} value={email} fluid />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={this.onChange}
-            value={password}
-            fluid
-          />
-        <button onClick={this.onSubmit}>Submit</button>
-      </div>
-    );
-  }
-
-  @action.bound
-  onChange(event) {
-    const { name, value } = event.target;
-    this[name] = value;
-  }
-
-  onSubmit() {
-    const { email, password } = this
-    console.log('결과확인 : ', email, password);
+      <Provider {...stores}>
+        <Router history={history}>
+          <Switch>
+            <Route exact={true} path="/" component={Login} />
+          </Switch>
+        </Router>
+      </Provider>
+    )
   }
 }
 
